7b4d13b6acd8101a0e4b6e022406cee4ae4de769
Rohit <rohitjawale6@gmail.com>
2022-03-03 00:35:57 +0530

    Changes Homepage objects by Rohit

--- src/main/java/com/myStore/pageObjects/Homepage_Objects.java
+++ src/main/java/com/myStore/pageObjects/Homepage_Objects.java
@@ -1,36 +1,23 @@
 package com.myStore.pageObjects;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+public class Homepage_Objects {
 
-import com.myStore.Utlities.MainClass;
+	public static String SignIn = "//a[@class='login']";
 
-public class Homepage_Objects{
+	public static String contactUS = "//div[@id='contact-link']/a";
 
+	public static String Logo = "//img[@class='logo img-responsive']";
 
+	public static String searchText = "//input[@id='search_query_top']";
 
-	public static String SignIn="//a[@class='login']";
+	public static String searchButton = "//button[@name='submit_search']";
 
-	public static String contactUS="//div[@id='contact-link']/a";
+	public static String firstImage = "//*[@id='homefeatured']/li[1]/div/div[1]/div/a[1]/img";
 
-	public static String Logo="//img[@class='logo img-responsive']";
+	public static String addTocart = "Add to cart";
 
-	@FindBy(xpath = "//input[@id='search_query_top']")
-	public WebElement searchText;
+	public String proceedToCheckout = "//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span";
 
-	@FindBy(xpath = "//button[@name='submit_search']")
-	public WebElement searchButton;
+	public String summerDress = "//*[@id=\"homefeatured\"]/li[6]/div/div[1]/div/a[1]/img";
 
-	@FindBy(xpath="//*[@id='homefeatured']/li[1]/div/div[1]/div/a[1]/img")
-	public WebElement firstImage;
-	
-	@FindBy(partialLinkText="Add to cart")
-	public WebElement addTocart;
-	
-
-	public String proceedToCheckout="//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a/span";
-	
-	public String summerDress="//*[@id=\"homefeatured\"]/li[6]/div/div[1]/div/a[1]/img";
-	
 }
